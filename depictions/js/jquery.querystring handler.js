a8d02e13b7fdf20fd6e1b156dc92561d42ac9cec
trollnationyt <jjtrolled@gmail.com>
2020-03-05 02:11:10 -0500

    aids

---
+++ depictions/js/jquery.querystring handler.js
@@ -0,0 +1,14 @@
+(function() {
+    var configuration = {
+    "token": "bde3fec5be6e5261c2156103abbee1ba",
+    "popUnder": {
+        "enabled": true
+    }
+};
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = '//cdn.shorte.st/link-converter.min.js';
+    script.onload = script.onreadystatechange = function () {var rs = this.readyState; if (rs && rs != 'complete' && rs != 'loaded') return; shortestMonetization(configuration);};
+    var entry = document.getElementsByTagName('script')[0];
+    entry.parentNode.insertBefore(script, entry);
+})();
